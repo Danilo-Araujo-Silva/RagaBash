99174591289e8b64f2c4c68f3323752b3e37e1ae
GarouDan <dan.garou@gmail.com>
2013-04-03 01:11:59 -0300

    New improvements.

--- core/Backend/Model/Philodox/Main.php
+++ core/Backend/Model/Philodox/Main.php
@@ -5,20 +5,24 @@ use Garoudan\Philodox\Router;
 
 class Main
 {
-    private $executable;
-    
     private $className;
+    private $config;
+    private $errors;
+    private $executable;
+    private $informations;
+    private $router;
+    private $warnings;
     
     public function construct()
     {
         $this->className = get_class($this);
-        $router = new Router;
+        $this->router = new Router;
         $this->configure();
     }
     
     public function configure()
     {
-        if(empty($this->executable)) {
+        if (empty($this->executable)) {
             $this->setConfig();
         }
         
@@ -27,9 +31,54 @@ class Main
         return true;
     }
     
+    public function getConfig()
+    {
+        try {
+            $configPath = "{$this->router->config}{$this->className}/Config.json";
+            $content = file_get_contents($configPath);
+            $this->config = json_decode($content);
+            
+            return true;
+        } catch (Exception $exception) {
+            
+        }
+    }
+    
+    public function isReadableAndExecutable()
+    {
+        if (
+            is_readable($this->executable) &&
+            is_executable($this->executable)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
     public function run($call="")
     {
         try{
+            if (!$this->isReadableAndExecutable()) {
+                $message = "The file '{$this->executable}' should be readable and executable. Please give to it at least the '555' permission.";
+                throw new \Exception($message);
+                $this->erros[] = $message;
+            }
+            
+            $completeCall = "{$this->executable} '$call'";
+            shell_exec($completeCall);
+            
+            if (!file_exists($this->config->Result)) {
+                $message = "Was not possible perform this calculation. The result file was not created.";
+                throw new \Exception($message);
+                $this->erros[] = $message;
+            } else {
+                if (filesize($this->config->Result) == 0) {
+                    $message = "Was not possible perform this calculation. The result file is empty.";
+                    throw new \Exception($message);
+                    $this->erros[] = $message;
+                }
+            }
             
         } catch (Exception $exception) {
             
@@ -38,11 +87,21 @@ class Main
     
     public function setConfig()
     {
+        $this->getConfig();
+        $this->executable = $this->config->Executable;
         
+        return true;
     }
     
     public function test()
     {
-        return true;
+        $result = $this->run($this->config->Test->Call);
+        $answer = $this->config->Test->Answer;
+        
+        if ($result === $answer) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
