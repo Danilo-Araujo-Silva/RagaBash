1552a002ac1583ef90f8204a7db3873d9623616e
GarouDan <dan.garou@gmail.com>
2013-04-03 22:23:44 -0300

    Testing new features.

--- RagaBash.php
+++ RagaBash.php
@@ -1,1 +1,27 @@
 <?php
+namespace GarouDan\RagaBash;
+
+use Garoudan\RagaBash\Core\Backend\Model;
+
+class RagaBash
+{
+    public static function engine($engine)
+    {
+        if (
+            in_array(
+                $engine,
+                array(
+                    "MatLab",
+                    "Maple",
+                    "Mathematica",
+                    "Octave"
+                )
+            )
+        ) {
+            $class = "Model\\$engine\\$engine";
+            return new $class;
+        } else {
+            return false;
+        }
+    }
+}
